f773e1c3e3a6a66ac0d8312faf577626a123bec5
Sam Griffin <sam.griffin@gmail.com>
2017-11-01 09:32:24 -0400

    make optimized runtime work with new grammar

--- src/SIL/RunTime.hs
+++ src/SIL/RunTime.hs
@@ -13,12 +13,13 @@ data RExpr
   | RPair !RExpr !RExpr
   | RVar
   | RCheck !RExpr !RExpr
-  | RApp !RExpr !RExpr
   | RGate !RExpr
   | RLeft !RExpr
   | RRight !RExpr
   | RTrace !RExpr
-  | RClosure !RExpr !RExpr
+  | RSetEnv !RExpr
+  | RDefer !RExpr
+  | RTwiddle !RExpr
   -- machine optimized instructions
   | RITE !RExpr !RExpr !RExpr
   | RChurch !Int !(Maybe RExpr)
@@ -28,13 +29,14 @@ instance EndoMapper RExpr where
   endoMap f RZero = f RZero
   endoMap f (RPair a b) = f $ RPair (endoMap f a) (endoMap f b)
   endoMap f RVar = f RVar
-  endoMap f (RApp c i) = f $ RApp (endoMap f c) (endoMap f i)
   endoMap f (RCheck c t) = f $ RCheck (endoMap f c) (endoMap f t)
   endoMap f (RGate x) = f . RGate $ endoMap f x
   endoMap f (RLeft x) = f . RLeft $ endoMap f x
   endoMap f (RRight x) = f . RRight $ endoMap f x
   endoMap f (RTrace x) = f . RTrace $ endoMap f x
-  endoMap f (RClosure c e) = f $ RClosure (endoMap f c) (endoMap f e)
+  endoMap f (RSetEnv x) = f . RSetEnv $ endoMap f x
+  endoMap f (RDefer x) = f . RDefer $ endoMap f x
+  endoMap f (RTwiddle x) = f . RTwiddle $ endoMap f x
   endoMap f (RITE i t e) = f $ RITE (endoMap f i) (endoMap f t) (endoMap f e)
   endoMap f r@(RChurch _ Nothing) = f r
 
@@ -47,45 +49,58 @@ toRExpr (Gate x) = RGate $ toRExpr x
 toRExpr (PLeft x) = RLeft $ toRExpr x
 toRExpr (PRight x) = RRight $ toRExpr x
 toRExpr (Trace x) = RTrace $ toRExpr x
-toRExpr _ = error "TODO toRexpr"
+toRExpr (SetEnv x) = RSetEnv $ toRExpr x
+toRExpr (Defer x) = RDefer $ toRExpr x
+toRExpr (Twiddle x) = RTwiddle $ toRExpr x
 
 fromRExpr :: RExpr -> IExpr
 fromRExpr RZero = Zero
 fromRExpr (RPair a b) = Pair (fromRExpr a) (fromRExpr b)
 fromRExpr RVar = Var
 fromRExpr (RCheck c t) = Check (fromRExpr c) (fromRExpr t)
-fromRExpr (RApp c i) = app (fromRExpr c) (fromRExpr i)
 fromRExpr (RGate x) = Gate $ fromRExpr x
 fromRExpr (RLeft x) = PLeft $ fromRExpr x
 fromRExpr (RRight x) = PRight $ fromRExpr x
 fromRExpr (RTrace x) = Trace $ fromRExpr x
+fromRExpr (RSetEnv x) = SetEnv $ fromRExpr x
+fromRExpr (RDefer x) = Defer $ fromRExpr x
+fromRExpr (RTwiddle x) = Twiddle $ fromRExpr x
 fromRExpr (RITE i t e) = app (Gate $ fromRExpr i) (Pair (fromRExpr e) (fromRExpr t))
 fromRExpr (RChurch i Nothing) = toChurch i
-fromRExpr _ = error "TODO fromRExpr"
 
-rEval f env g = let f' = f env in case g of
+rEval f env g = let f' = f env
+                    rApply (RPair ng eenv) v = f (RPair v eenv) ng
+                    rApply (RChurch ci Nothing) v = pure $ RChurch ci (Just v)
+                    rApply (RChurch ci (Just cf)) v =
+                      let step 0 cv = pure cv
+                          step x cv = rApply cf cv >>= step (x - 1)
+                      in step ci v
+                    rApply ng _ = fail $ "not a closure " ++ show ng
+                in case g of
   RZero -> pure RZero
   (RPair a b) -> RPair <$> f' a <*> f' b
   RVar -> pure env
   RCheck c t -> do
-    tcResult <- f' (RApp t c)
+    -- tcResult <- f' (RApp t c)
+    tcResult <- f' (RSetEnv (RTwiddle (RPair c t)))
     case tcResult of
       RZero -> f' c
       err -> fail $ concat ["failed rrefinement check for ", show c, " -- ",  show err]
-  RApp g i -> let rApply (RClosure ng eenv) v = f (RPair v eenv) ng
-                  rApply (RChurch ci Nothing) v = pure $ RChurch ci (Just v)
-                  rApply (RChurch ci (Just cf)) v =
-                    let step 0 cv = pure cv
-                        step x cv = rApply cf cv >>= step (x - 1)
-                    in step ci v
-                  rApply ng _ = fail $ "not a closure " ++ show ng
-              in do
+  RDefer x -> pure x
+  RTwiddle x -> f' x >>= \nx -> case nx of
+    RPair i (RPair c cenv) -> pure $ RPair c (RPair i cenv)
+    bx -> fail $ concat ["rtwiddle: expecting pair pair, got ", show nx]
+  -- this seems a bit hacky
+  RSetEnv (RTwiddle (RPair i g)) -> do
     ng <- f' g
     ni <- f' i
     rApply ng ni
+  RSetEnv x -> f' x >>= \g -> case g of
+    RPair c i -> rApply c i
+    bx -> fail $ concat ["rsetenv basic - not a closure: ", show bx]
   RGate x -> f' x >>= \g -> case g of
-    RZero -> pure $ RClosure (RLeft (RLeft RVar)) RZero
-    _ -> pure $ RClosure (RRight (RLeft RVar)) RZero
+    RZero -> pure $ RLeft RVar
+    _ -> pure $ RRight RVar
   RLeft g -> f' g >>= \g -> case g of
     (RPair a _) -> pure a
     _ -> pure RZero
@@ -93,8 +108,6 @@ rEval f env g = let f' = f env in case g of
     (RPair _ b) -> pure b
     _ -> pure RZero
   RTrace g -> f' g >>= \g -> pure $ trace (show g) g
-  RClosure c RZero -> pure $ RClosure c env
-  RClosure _ e -> fail $ concat ["unexpected closure with environment ", show e]
   RITE i t e -> f' i >>= \ng -> case ng of
     RZero -> f' e
     _ -> f' t
@@ -143,14 +156,14 @@ toChurch x =
 
 rOptimize :: RExpr -> RExpr
 rOptimize =
-  let modR (RApp (RGate i) (RPair e t)) = RITE i t e
-      modR c@(RClosure (RClosure apps RZero) RZero) = let appCount = countApps 0 apps in
+  let modR (RSetEnv (RPair (RGate i) (RPair e t))) = RITE i t e
+      modR c@(RPair (RDefer (RPair (RDefer (apps)) RVar)) RVar) = let appCount = countApps 0 apps in
         if appCount > 0
         then RChurch appCount Nothing
         else c
       modR x = x
       countApps x (RLeft RVar) = x
-      countApps x (RApp (RLeft (RRight RVar)) ia) = countApps (x + 1) ia
+      countApps x (RSetEnv (RTwiddle (RPair ia (RLeft (RRight RVar))))) = countApps (x + 1) ia
       countApps _ _ = 0
   in endoMap modR
 
@@ -163,7 +176,7 @@ fasterEval =
   in fmap fromRExpr . frEval . rOptimize . toRExpr
 
 optimizedEval :: IExpr -> IO IExpr
-optimizedEval = simpleEval -- fasterEval --simpleEval . optimize
+optimizedEval = fasterEval --simpleEval . optimize
 
 pureEval :: IExpr -> IExpr
 pureEval g = runIdentity $ fix iEval Zero g
@@ -191,10 +204,11 @@ fullEval typeCheck i = typedEval typeCheck i print
 
 prettyEval typeCheck i = typedEval typeCheck i (print . PrettyIExpr)
 
+-- TODO fix tictactoe type checking
 evalLoop :: (Show a, Eq a) => (DataType -> IExpr -> Maybe a) -> IExpr -> IO ()
-evalLoop typeCheck iexpr = if typeCheck (ArrType ZeroType ZeroType) iexpr == Nothing
+evalLoop typeCheck iexpr = if typeCheck (PairType (ArrType ZeroType ZeroType) ZeroType) iexpr == Nothing
   then let mainLoop s = do
-             result <- fmap fromRExpr . fix rEval RZero $ RApp optIExpr (toRExpr s)
+             result <- fmap fromRExpr . fix rEval RZero $ RSetEnv (RTwiddle (RPair (toRExpr s) optIExpr))
              case result of
                Zero -> putStrLn "aborted"
                (Pair disp newState) -> do
@@ -208,4 +222,4 @@ evalLoop typeCheck iexpr = if typeCheck (ArrType ZeroType ZeroType) iexpr == Not
            optIExpr = rOptimize $ toRExpr iexpr
        in mainLoop Zero
   else putStrLn $ concat ["main's inferred type: "
-                         , show $ typeCheck (ArrType ZeroType ZeroType) iexpr]
+                         , show $ typeCheck  (PairType (ArrType ZeroType ZeroType) ZeroType) iexpr]
