6b0bc28c894c40d5dd8db5dbc8585e7471852a38
hhefesto <hhefesto@rdataa.com>
2020-10-14 13:25:34 -0500

    derive-storable-plugin successfully integrated

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 { # Fetch the latest haskell.nix and import its default.nix
-  haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/c88f9eccc975b21ae1e6a6b8057a712b91e374f2.tar.gz") {}
+  haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/ef6ca0f431fe3830c25cb2d185367245c1cce894.tar.gz") {}
   # haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz") {}
 
   # # For LLVM
@@ -18,20 +18,21 @@ let
   # patches and also the haskell.nix functionality itself as an overlay.
   nixpkgsArgs = haskellNix.nixpkgsArgs;
 
-  telomareOverlays = [ (self: super: {
-    jumper = telomare_jumper;
-    gc = self.boehmgc;
-    llvm-config = self.llvm_9;
-  }) ];
-
-  # import nixpkgs with overlays
-  pkgs = (import nixpkgsSrc (nixpkgsArgs // { overlays = nixpkgsArgs.overlays ++ telomareOverlays;}));
-
   telomare_jumper = pkgs.stdenv.mkDerivation {
     name = "telomareJumper";
     src = ./cbits;
     buildInputs = [ pkgs.boehmgc ];
   };
+
+  telomareOverlays = [ (self: super: {
+    jumper = telomare_jumper;
+    gc = self.boehmgc;
+    llvm-config = self.llvm_9;
+    alex = self.haskellPackages.alex;
+  }) ];
+
+  # import nixpkgs with overlays
+  pkgs = (import nixpkgsSrc (nixpkgsArgs // { overlays = nixpkgsArgs.overlays ++ telomareOverlays;}));
 in
 pkgs.haskell-nix.cabalProject {
   src = pkgs.haskell-nix.cleanSourceHaskell {
@@ -43,8 +44,9 @@ pkgs.haskell-nix.cabalProject {
      (hackage: {
        llvm-hs = hackage.llvm-hs."9.0.1".revisions.default;
        llvm-hs-pure = hackage.llvm-hs-pure."9.0.0".revisions.default;
-       derive-storable-plugin = hackage.derive-storable-plugin."0.2.3.0".revisions.default;
      })
    ];
-  modules = [];
+  modules = [
+    { reinstallableLibGhc = true; }
+  ];
 }
