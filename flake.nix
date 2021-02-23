4ab8aa2e696c2e0aa8f844488907971bbc1995de
hhefesto <hhefesto@rdataa.com>
2021-02-23 14:05:38 -0600

    CI test 3

--- flake.nix
+++ flake.nix
@@ -1,8 +1,14 @@
 {
   description = "Nix flake for Telomare";
+
+  inputs.flake-compat = {
+    url = "github:edolstra/flake-compat";
+    flake = false;
+  };
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
     # flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
