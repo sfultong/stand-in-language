c60039c3b5fc99bbe520bf029de20c4a9024a049
hhefesto <hhefesto@rdataa.com>
2021-02-23 14:10:40 -0600

    CI test 4

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+  outputs = { self, nixpkgs, flake-utils, haskellNix, flake-compat }:
     # flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
