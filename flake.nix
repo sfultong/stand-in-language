1f89c7395b909b63c1c038231bf97e49881484c7
hhefesto <hhefesto@rdataa.com>
2021-03-05 12:06:13 -0600

    check try n + 1

--- flake.nix
+++ flake.nix
@@ -10,7 +10,6 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils, haskellNix, flake-compat }:
-    # flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
       overlays = [ haskellNix.overlay
@@ -54,6 +53,13 @@
     in flake // {
       # Built by `nix build .`
       defaultPackage = flake.packages."telomare:exe:telomare-exe";
+      telomareTests = flake.packages."telomare:test:telomare-test";
+
+      checks.x86_64-linux = {
+
+        build = self.defaultPackage.x86_64-linux;
+      };
+
 
       # This is used by `nix develop .` to open a shell for use with
       # `cabal`, `hlint` and `haskell-language-server`
